57953651ad5da3323b6aaf6c04df21ff8c9f642f
Upwriter <contato@upwriter.com.br>
2025-09-29 19:23:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ],
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
